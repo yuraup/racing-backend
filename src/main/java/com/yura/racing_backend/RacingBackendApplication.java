78cb539443e2c17191b495cbaa7101f915e54b0a
yuraup <o721@naver.com>
2025-11-14 08:21:18 +0900

    chore: spring boot 프로젝트 초기 세팅

---
+++ src/main/java/com/yura/racing_backend/RacingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yura.racing_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yura/racing_backend/RacingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yura.racing_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
